d14d246ed3f32f51230318f7694fc38ea58ce3d8
nandalalainfotech <nandalalainfotech@gmail.com>
2022-11-18 15:42:35 +0530

    Data Grid for Tables done by Aravinth.

--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -1,36 +1,42 @@
-/* eslint-disable no-array-constructor */
-import React, { useEffect } from "react";
+import { deepPurple, red } from "@material-ui/core/colors";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Box from "@mui/material/Box";
+import { alpha, styled } from "@mui/material/styles";
+import { DataGrid, gridClasses } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import {
-  createProduct,
-  deleteProduct,
-  listProducts,
-} from "../actions/productAction";
+import { createProduct, deleteProduct, listProducts } from "../actions/productAction";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import {
-  PRODUCT_CREATE_RESET,
-  PRODUCT_DELETE_RESET,
-} from "../constants/productConstants";
-import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import { PRODUCT_CREATE_RESET, PRODUCT_DELETE_RESET, PRODUCT_DETAILS_RESET, PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
-export default function ProductListScreen(props) {
+
+export default function ProductListScreen() {
   const navigate = useNavigate();
   const { pageNumber = 1 } = useParams();
-  const pageSize = 15;
   const { pathname } = useLocation();
-  const sellerMode = pathname.indexOf('/seller') >= 0;
+  const sellerMode = pathname.indexOf("/seller") >= 0;
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products, page, pages } = productList;
+
+  const [pageSize, setPageSize] = useState(10);
+
+  const { loading, error, products } = productList;
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product } = productDetails;
+
   const productCreate = useSelector((state) => state.productCreate);
-  console.log("called----->productCreate", productCreate);
   const {
     loading: loadingCreate,
     error: errorCreate,
     success: successCreate,
     product: createdProduct,
   } = productCreate;
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const { success: successUpdate } = productUpdate;
   const productDelete = useSelector((state) => state.productDelete);
   const {
     loading: loadingDelete,
@@ -44,13 +50,18 @@ export default function ProductListScreen(props) {
   useEffect(() => {
     if (successCreate) {
       dispatch({ type: PRODUCT_CREATE_RESET });
-      navigate(`/product/${createdProduct._id}/edit`);
+    }
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+    }
+    if (product) {
+      dispatch({ type: PRODUCT_DETAILS_RESET });
     }
     if (successDelete) {
       dispatch({ type: PRODUCT_DELETE_RESET });
     }
     dispatch(
-      listProducts({ seller: sellerMode ? userInfo._id : '', pageNumber })
+      listProducts({ seller: sellerMode ? userInfo._id : "", pageNumber })
     );
   }, [
     createdProduct,
@@ -58,26 +69,126 @@ export default function ProductListScreen(props) {
     navigate,
     sellerMode,
     successCreate,
+    successUpdate,
     successDelete,
     userInfo._id,
     pageNumber,
+    product,
   ]);
+
   const deleteHandler = (product) => {
     if (window.confirm("Are you sure to delete?")) {
-      dispatch(deleteProduct(product._id));
+      dispatch(deleteProduct(product.row._id));
     }
   };
-  console.log("called----->createProduct", createProduct);
-  const createHandler = () => {
+
+   const createHandler = () => {
     dispatch(createProduct());
   };
+
+  const editHandler = (product) => {
+    console.log("product", product);
+    navigate(`/product/${product.row._id}/edit`);
+  };
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "name",
+      headerName: "NAME",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "price",
+      headerName: "PRICE",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "category",
+      headerName: "CATEGORY",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "categorygroup",
+      headerName: "CATEGORY GROUP",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "brand",
+      headerName: "BRAND",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+    },
+    {
+      field: "actions",
+      headerName: "ACTIONS",
+      flex: 1,
+      headerClassName: "super-app-theme--header",
+      renderCell: (products) => (
+        <>
+          <EditIcon
+            onClick={() => editHandler(products)}
+            style={{ color: deepPurple[500], fontSize: 15, margin:20, cursor: "pointer" }}
+          />
+
+          <DeleteIcon
+            onClick={() => deleteHandler(products)}
+            style={{ color: red[500], fontSize: 15, cursor: "pointer"}}
+          />
+        </>
+      ),
+    },
+  ];
+
+  const ODD_OPACITY = 0.2;
+
+  const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+    [`& .${gridClasses.row}.even`]: {
+      backgroundColor: theme.palette.grey[200],
+      "&:hover, &.Mui-hovered": {
+        backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
+        "@media (hover: none)": {
+          backgroundColor: "transparent",
+        },
+      },
+      "&.Mui-selected": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          ODD_OPACITY + theme.palette.action.selectedOpacity
+        ),
+        "&:hover, &.Mui-hovered": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            ODD_OPACITY +
+              theme.palette.action.selectedOpacity +
+              theme.palette.action.hoverOpacity
+          ),
+          // Reset on touch devices, it doesn't add specificity
+          "@media (hover: none)": {
+            backgroundColor: alpha(
+              theme.palette.primary.main,
+              ODD_OPACITY + theme.palette.action.selectedOpacity
+            ),
+          },
+        },
+      },
+    },
+  }));
+
   return (
     <div>
-      <div className="row productsLists">
-        <h1>Products</h1>
-        <button type="button" className="primary" onClick={createHandler}>
-          Create Product
-        </button>
+      <div>
+        <h1 >Products</h1>
+        <Button  variant="contained" onClick={createHandler}>Create Product</Button>
       </div>
       {loadingDelete && <LoadingBox></LoadingBox>}
       {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
@@ -88,89 +199,50 @@ export default function ProductListScreen(props) {
       ) : error ? (
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
-        <>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>CATEGORY</th>
-                <th>BRAND</th>
-                <th>ACTIONS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product._id}>
-                  <td>{product._id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.category}</td>
-                  <td>{product.brand}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() =>
-                        navigate(`/product/${product._id}/edit`)
-                      }
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() => deleteHandler(product)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-
-
-              {/* {sarees.map((saree) => (
-                <tr key={saree._id}>
-                  <td>{saree._id}</td>
-                  <td>{saree.name}</td>
-                  <td>{saree.price}</td>
-                  <td>{saree.category}</td>
-                  <td>{saree.brand}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() =>
-                        navigate(`/saree/${saree._id}/edit`)
-                      }
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() => deleteHandler(saree)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))} */}
-            </tbody>
-          </table>
-          <div className="row center pagination">
-            {[...Array(pages, pageSize).keys()].map((x) => (
-              <Link
-                className={x + 1 === page ? 'active' : ''}
-                key={x + 1}
-                to={`/productlist/pageNumber/${x + 1}`}
-              >
-                {x + 1}
-              </Link>
-            ))}
-          </div>
-        </>
+        <Box
+          sx={{
+            height: 460,
+            width: "100%",
+            "& .super-app-theme--header": {
+              backgroundColor: "#808080",
+              color: "#ffffff",
+            },
+            "& .css-1jbbcbn-MuiDataGrid-columnHeaderTitle": {
+              fontSize: 16,
+            },
+            ".css-o8hwua-MuiDataGrid-root .MuiDataGrid-cellContent": {
+              fontSize: 13,
+            },
+            ".css-bfht93-MuiDataGrid-root .MuiDataGrid-columnHeader--alignCenter .MuiDataGrid-columnHeaderTitleContainer":
+              {
+                backgroundColor: "#330033",
+                color: "#ffffff",
+              },
+            ".css-h4y409-MuiList-root": {
+              display: "grid",
+            },
+          }}
+        >
+          <StripedDataGrid
+            sx={{
+              boxShadow: 10,
+              borderRadius: 0,
+              m: 2,
+            }}
+            columns={columns}
+            rows={products}
+            getRowId={(rows) => rows._id}
+            VerticalAlignment="Center"
+            rowHeight={34}
+            getRowClassName={(params) =>
+              params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+            }
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[5, 10, 20]}
+            pagination
+          />
+        </Box>
       )}
     </div>
   );
